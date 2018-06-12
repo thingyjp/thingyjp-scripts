4161505dc2d5144aad39d0316096e65343c32434
Daniel Palmer <daniel@0x0f.com>
2018-06-13 02:05:04 +0900

    Add bootstrap script. Clean up CA setup script a bit.

--- setupca.sh
+++ setupca.sh
@@ -10,13 +10,25 @@ easyrsa_pki_server_init
 easyrsa_pki_device_init
 
 createsubca () {
-	EASYRSA_PKI=$1 $EASYRSA --batch --req-cn="$2" build-ca nopass subca
-	EASYRSA_PKI=$EASYRSA_PKI_ROOT $EASYRSA --batch import-req $1/reqs/ca.req $3
-	EASYRSA_PKI=$EASYRSA_PKI_ROOT $EASYRSA --batch sign-req ca $3
-	cp $EASYRSA_PKI_ROOT/issued/$3.crt $1/ca.crt
+	{
+		$EASYRSA --pki-dir=$1 \
+			 --batch \
+		 	 --req-cn="$2" \
+		 	 build-ca nopass subca
+		$EASYRSA --pki-dir=$EASYRSA_PKI_ROOT \
+			 --batch \
+		 	import-req $1/reqs/ca.req $3
+		$EASYRSA --pki-dir=$EASYRSA_PKI_ROOT \
+			 --batch \
+		 	sign-req ca $3
+		cp $EASYRSA_PKI_ROOT/issued/$3.crt $1/ca.crt
+	} &>> $LOGFILE
 }
 
-EASYRSA_PKI=$EASYRSA_PKI_ROOT $EASYRSA --batch --req-cn="thingy.jp root CA" build-ca nopass
+$EASYRSA --pki-dir=$EASYRSA_PKI_ROOT \
+	 --batch \
+	 --req-cn="thingy.jp root CA" \
+	 build-ca nopass
 
 createsubca $EASYRSA_PKI_SERVER "thingy.jp server CA" "serverca"
-createsubca $EASYRSA_PKI_DEVICE	"thingy.jp device CA" "deviceca"
+createsubca $EASYRSA_PKI_DEVICE "thingy.jp device CA" "deviceca"
