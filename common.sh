448b69640dbc6d011813bb40e2b0820541623df3
Daniel Palmer <daniel@0x0f.com>
2018-10-22 21:19:17 +0900

    Use git to track pki changes

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ if [ -z "$THINGYJP_HOME" ]; then
 fi
 
 THINGYJP_ROOTCERT=$THINGYJP_HOME/thingyjp_root.crt
-THINGYJP_SELFSERVICEURL="https://localhost:5000"
+THINGYJP_SELFSERVICEURL="http://selfservice.thingy.jp"
 
 EASYRSA=./easy-rsa/easyrsa3/easyrsa
 EASYRSA_PKI_USER="$THINGYJP_HOME/pki_user"
@@ -18,32 +18,30 @@ init () {
 	fi
 }
 
-easyrsa_pki_user_init () {
-	if [ ! -d "$EASYRSA_PKI_USER" ]; then
-		echo "user pki doesn't exist, creating..."
-		EASYRSA_PKI=$EASYRSA_PKI_USER $EASYRSA init-pki >> $LOGFILE
+easyrsa_pki_init () {
+	if [ ! -d "$2" ]; then
+		echo "$1 pki doesn't exist, creating..."
+		EASYRSA_PKI=$2 $EASYRSA init-pki >> $LOGFILE
+		git -C $2 init
+		git -C $2 add -A
+		git -C $2 commit --allow-empty -a -m "create pki"
 	fi
+}
+
+easyrsa_pki_user_init () {
+	easyrsa_pki_init "user" $EASYRSA_PKI_USER
 }
 
 easyrsa_pki_root_init () {
-	if [ ! -d "$EASYRSA_PKI_ROOT" ]; then
-		echo "root pki doesn't exist, creating..."
-		EASYRSA_PKI=$EASYRSA_PKI_ROOT $EASYRSA init-pki >> $LOGFILE
-	fi
+	easyrsa_pki_init "root" $EASYRSA_PKI_ROOT
 }
 
 easyrsa_pki_server_init () {
-	if [ ! -d "$EASYRSA_PKI_SERVER" ]; then
-		echo "server pki doesn't exist, creating..."
-		EASYRSA_PKI=$EASYRSA_PKI_SERVER $EASYRSA init-pki >> $LOGFILE
-	fi
+	easyrsa_pki_init "server" $EASYRSA_PKI_SERVER
 }
 
 easyrsa_pki_device_init () {
-        if [ ! -d "$EASYRSA_PKI_DEVICE" ]; then
-                echo "device pki doesn't exist, creating..."
-                EASYRSA_PKI=$EASYRSA_PKI_DEVICE $EASYRSA init-pki >> $LOGFILE
-        fi
+	easyrsa_pki_init "device" $EASYRSA_PKI_DEVICE
 }
 
 easyrsa_pki_device_check () {
@@ -60,6 +58,20 @@ easyrsa_pki_server_check () {
 	fi
 }
 
+easyrsa_pki_stamp () {
+	git -C $2 add -A
+	git -C $2 commit --allow-empty -a -m "$1"
+}
+
+easyrsa_pki_abort () {
+	git -C $1 reset HEAD^
+}
+
 easyrsa_csr_device_create () {
 	EASYRSA_PKI=$EASYRSA_PKI_USER $EASYRSA --batch --req-cn=$1 gen-req $1 nopass >> $LOGFILE
+	easyrsa_pki_stamp "creating csr for $1" $EASYRSA_PKI_USER
+}
+
+easyrsa_csr_device_abort () {
+	easyrsa_pki_abort $EASYRSA_PKI_USER
 }
--- device_requestcert.sh
+++ device_requestcert.sh
@@ -17,4 +17,15 @@ easyrsa_csr_device_create $UUID
 #TODO send request to self service here
 
 JSON=`jo csr="$(cat $EASYRSA_PKI_USER/reqs/$UUID.req)"`
-curl --cacert "$THINGYJP_ROOTCERT" -H "Content-Type: application/json" -d "$JSON" "$THINGYJP_SELFSERVICEURL/device/commission"
+
+set +e
+curl --fail \
+	--cacert "$THINGYJP_ROOTCERT" \
+	-H "Content-Type: application/json" \
+	-d "$JSON" "$THINGYJP_SELFSERVICEURL/device/commission"
+if [ "$?" -ne "0" ]; then
+	echo "failed to request cert"
+	easyrsa_csr_device_abort
+	exit 1
+fi
+set -e
